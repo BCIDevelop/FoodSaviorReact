4655c7bf9002404f14ecb439b0b6c335bbae6d30
Luis Lopez <luis.lopez@utec.edu.pe>
2022-12-18 00:49:35 -0500

    gmail login button register

--- src/components/social-button-user/SocialButtons.jsx
+++ src/components/social-button-user/SocialButtons.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { facebookClicked,loginGmail } from '../../utils/socialButtons'
 import faceLogo from '../../assets/facebook-svgrepo-com.svg'
 import styles from './social.module.css'
 import { useEffect } from 'react'
+import {AlertContext} from '../../context/AlertContext'
+import { useNavigate} from 'react-router-dom'
+import {UserContext} from '../../context/UserContext'
+
 const SocialButtons = () => {
+  const {showToast} =useContext(AlertContext)
+  const {storeUser}=useContext(UserContext)
+  const history = useNavigate()
   function loginGmail(response){
-    console.log('entro')
     console.log(response)
       const decoded_jwt=jwtJsDecode.jwtDecode(response.credential)
       const socialUser={name:decoded_jwt.payload.name,username:decoded_jwt.payload.email,picture:decoded_jwt.payload.picture}
-      return socialUser
-    }
-    window.onload = function () {
-      console.log('entroon')
-        
-       
+      const users=JSON.parse(localStorage.getItem('users'))?JSON.parse(localStorage.getItem('users')): []
+      if(users.some((element)=>{element.mail===socialUser.username})) 
+      {
+            storeUser(socialUser)
       }
+      else {
+        users.push(socialUser)
+        localStorage.setItem('users',JSON.stringify(users))
+        storeUser(socialUser)
+      }
+      history('/home')
+    }
+   
    useEffect(()=>{
-    console.log('entroUse')
     google.accounts.id.initialize({
-      client_id: "867773923360-1o9vafk85bmvsnh2oeebjg7v7ks73us9.apps.googleusercontent.com",
-      callback: loginGmail
-    });
+          client_id: "867773923360-1o9vafk85bmvsnh2oeebjg7v7ks73us9.apps.googleusercontent.com",
+          callback: loginGmail
+        });
     google.accounts.id.renderButton(
       document.getElementById("gmail-button"),
       { theme: "filled_blue", size: "large",type:"icon",shape:"circle" }  // customization attributes
