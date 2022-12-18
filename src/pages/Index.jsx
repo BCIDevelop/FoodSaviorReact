6c63092c2f837818c84a11851e4a679054c71439
Brayan Jhonn <bj27@users.noreply.github.com>
2022-12-18 16:19:24 -0500

    se agrega diseño inventario

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,11 +3,12 @@ import Register from "./register/Register";
 import NotFound from "./404/NotFound";
 import Home from "./home/Home";
 import Product from "./product/Product";
+import Inventory from "./inventory/Inventory";
 import Category from "./category/Category";
 import FoodSavior from "./app/FoodSavior";
 import DashboardTest from "./dashboardTest/Dashboard";
 import AlertPage from "./alert/AlertPage";
 import Favorites from "./favorites/Favorites";
-export {Login,Register,Product,Home,NotFound,Category,FoodSavior,DashboardTest,AlertPage,Favorites}
-export const indexPagesPrivate=['Home','DashBoard','Category','Product','Favorites']
+export {Login,Register,Product, Inventory,Home,NotFound,Category,FoodSavior,DashboardTest,AlertPage,Favorites}
+export const indexPagesPrivate=['Home','DashBoard','Category','Product', 'Inventory','Favorites']
 export const indexPagesPublic =['Login']
---
+++ src/pages/inventory/Inventory.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import CRUDInventory from '../../components/crud/CRUDInventory'
+
+const Inventory = () => {
+  return (
+    <CRUDInventory />
+  )
+}
+
+export default Inventory
