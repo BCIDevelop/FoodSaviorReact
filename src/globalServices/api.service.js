814a702896dbbaecdfa09e888b73b5f1e41061ef
Luis Lopez <luis.lopez@utec.edu.pe>
2023-03-11 15:23:12 -0500

    url backend added

--- src/globalServices/api.service.js
+++ src/globalServices/api.service.js
@@ -1,6 +1,6 @@
 const makeRequest = async (context,method,data,hasCredentials,bodyFormat="row")=>{
     try{
-        const url='http://127.0.0.1:5000'
+        const url='https://foodsavior.onrender.com/'
         const myHeaders = new Headers();
         if (bodyFormat==='row'){
             myHeaders.append("Content-Type", "application/json");
