0323a134edcdceec3b57d75c442d3dcff810e6a8
Luis Lopez <luis.lopez@utec.edu.pe>
2023-06-07 18:47:25 -0500

    service signal implemtation

--- src/globalServices/categories.service.js
+++ src/globalServices/categories.service.js
@@ -1,9 +1,9 @@
 import makeRequest from "./api.service"
 import responseHandler from "../utils/handlerResponse"
 
-export const setCategoriesUpdate=async(history,showToast,removeUser,id, formContent)=>{
-  const response=await makeRequest(`categories/${id}`,'PUT',formContent,true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const setCategoriesUpdate=async(signal,history,showToast,removeUser,id, formContent)=>{
+  const response=await makeRequest(signal,`categories/${id}`,'PUT',formContent,true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
  
     return response.results    
@@ -14,49 +14,49 @@ export const setCategoriesUpdate=async(history,showToast,removeUser,id, formCont
     }
 }
 
-export const setCategoriesCreate=async(history,showToast,removeUser, formContent)=>{
-  const response=await makeRequest(`categories`,'POST',formContent,true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const setCategoriesCreate=async(signal,history,showToast,removeUser, formContent)=>{
+  const response=await makeRequest(signal,`categories`,'POST',formContent,true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
     return response.results    
   }
   else{
-      const response=await makeRequest('categories','GET',{},true)
+      const response=await makeRequest(signal,'categories','GET',{},true)
       return response.results.results
     }
 }
 
-export const getCategoryById=async(history,showToast,removeUser,id)=>{
-  const response=await makeRequest(`categories/${id}`,'GET',{},true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const getCategoryById=async(signal,history,showToast,removeUser,id)=>{
+  const response=await makeRequest(signal,`categories/${id}`,'GET',{},true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
  
     return response.results    
   }
   else{
-      const response=await makeRequest('categories','GET',{},true)
+      const response=await makeRequest(signal,'categories','GET',{},true)
       return response.results.results
     }
 }
 
-export const getCategories=async(history,showToast,removeUser,perPage = 5)=>{
-  const response=await makeRequest(`categories?page=1&per_page=${perPage}`,'GET',{},true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const getCategories=async(signal,history,showToast,removeUser,perPage = 5)=>{
+  const response=await makeRequest(signal,`categories?page=1&per_page=${perPage}`,'GET',{},true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  return response.results
   else{
-      const response=await makeRequest('categories','GET',{},true)
+      const response=await makeRequest(signal,'categories','GET',{},true)
       return response.results.results
     }
 }
 
-export const setCategories=async(history,showToast,removeUser,formData)=>{
-  const response=await makeRequest(`categories`,'POST',formData,true,"form-data")
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const setCategories=async(signal,history,showToast,removeUser,formData)=>{
+  const response=await makeRequest(signal,`categories`,'POST',formData,true,"form-data")
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
     return response.results
   }
   else{
-      const response=await makeRequest('products','GET',{},true)
+      const response=await makeRequest(signal,'products','GET',{},true)
       return response.results.results
     }
 }
--- src/globalServices/products.service.js
+++ src/globalServices/products.service.js
@@ -1,66 +1,66 @@
 import makeRequest from "./api.service"
 import responseHandler from "../utils/handlerResponse"
 
-export const setProductUpdateById=async(history,showToast,removeUser,id, formData)=>{
-  const response=await makeRequest(`products/${id}`,'PATCH',formData,true,"form-data")
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const setProductUpdateById=async(signal,history,showToast,removeUser,id, formData)=>{
+  const response=await makeRequest(signal,`products/${id}`,'PATCH',formData,true,"form-data")
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
     return response.results    
   }
   else{
-      const response=await makeRequest('products','GET',{},true)
+      const response=await makeRequest(signal,'products','GET',{},true)
       return response.results.results
     }
 }
 
-export const setProductByIdAndUser=async(history,showToast,removeUser,formData)=>{
-  const response=await makeRequest(`products/user`,'POST',formData,true,"form-data")
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const setProductByIdAndUser=async(signal,history,showToast,removeUser,formData)=>{
+  const response=await makeRequest(signal,`products/user`,'POST',formData,true,"form-data")
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
     return response.results    
   }
   else{
-      const response=await makeRequest('products','GET',{},true)
+      const response=await makeRequest(signal,'products','GET',{},true)
       return response.results.results
     }
 }
 
-export const getProductById=async(history,showToast,removeUser,id)=>{
-  const response=await makeRequest(`products/${id}`,'GET',{},true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const getProductById=async(signal,history,showToast,removeUser,id)=>{
+  const response=await makeRequest(signal,`products/${id}`,'GET',{},true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
    
     return response.results    
   }
   else{
-      const response=await makeRequest('products','GET',{},true)
+      const response=await makeRequest(signal,'products','GET',{},true)
       return response.results.results
     }
 }
 
-export const getAllProductByUser=async(history,showToast,removeUser, paramGET)=>{
-  const response=await makeRequest('products/user?page='+paramGET.page,'GET',{},true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const getAllProductByUser=async(signal,history,showToast,removeUser, paramGET)=>{
+  const response=await makeRequest(signal,'products/user?page='+paramGET.page,'GET',{},true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  {
     return response.results;    
   }
   else{
-      const response=await makeRequest('products','GET',{},true)
+      const response=await makeRequest(signal,'products','GET',{},true)
       return response.results.results
     }
 }
 
-export const getAllProductsService=async(history,showToast,removeUser,perPage = 5)=>{
-  const response=await makeRequest(`products?page=1&per_page=${perPage}`,'GET',{},true)
-  const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const getAllProductsService=async(signal,history,showToast,removeUser,perPage = 5)=>{
+  const response=await makeRequest(signal,`products?page=1&per_page=${perPage}`,'GET',{},true)
+  const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
   if (isResponseValid)  return response.results.results    
   else{
-      const response=await makeRequest('products','GET',{},true)
+      const response=await makeRequest(signal,'products','GET',{},true)
       return response.results.results
     }
 }
 
-export const getProductsService=async(history,showToast,removeUser,perPage,orderBySpoilDate)=>{
+export const getProductsService=async(signal,history,showToast,removeUser,perPage,orderBySpoilDate)=>{
    const controller=new AbortController()
    const signal=controller.signal
     const response=await makeRequest(signal,`products/user?page=1&per_page=${perPage}&ordering_by_spoilDate=${orderBySpoilDate}`,'GET',{},true)
@@ -72,22 +72,22 @@ export const getProductsService=async(history,showToast,removeUser,perPage,order
       }
 }
 
-export const deleteProductService=async(history,showToast,removeUser,productId)=>{
-    const response=await makeRequest(`products/${productId}`,'DELETE',{},true)
-    const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const deleteProductService=async(signal,history,showToast,removeUser,productId)=>{
+    const response=await makeRequest(signal,`products/${productId}`,'DELETE',{},true)
+    const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
     if (isResponseValid)  return response.results.results    
     else{
-        const response=await makeRequest(`products/${productId}`,'DELETE',{},true)
+        const response=await makeRequest(signal,`products/${productId}`,'DELETE',{},true)
         return response.results.results
       }
 }
 
-export const getProductsByCategoryService=async(history,showToast,removeUser,categoryId,perPage)=>{
-    const response=await makeRequest(`products/user?page=1&per_page=${perPage}&category_id=${categoryId}`,'GET',{},true)
-    const isResponseValid=await responseHandler(response,history,showToast,removeUser)
+export const getProductsByCategoryService=async(signal,history,showToast,removeUser,categoryId,perPage)=>{
+    const response=await makeRequest(signal,`products/user?page=1&per_page=${perPage}&category_id=${categoryId}`,'GET',{},true)
+    const isResponseValid=await responseHandler(signal,response,history,showToast,removeUser)
     if (isResponseValid)  return response.results.results    
     else{
-        const response=await makeRequest(`products/user?page=1&per_page=${perPage}&category_id=${categoryId}`,'GET',{},true)
+        const response=await makeRequest(signal,`products/user?page=1&per_page=${perPage}&category_id=${categoryId}`,'GET',{},true)
         return response.results.results
       }
 }
